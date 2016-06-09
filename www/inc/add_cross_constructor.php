612889fccbd3fa9347874c6b0294c67a56c7af37
Dima Kovalev <kacevnik@gmail.com>
2016-06-09 16:58:41 +0300

    Реализована функция отправки письма для проверки добавленного кроссворда

--- www/inc/add_cross_constructor.php
+++ www/inc/add_cross_constructor.php
@@ -22,10 +22,11 @@
     if(!$name){echo "<div class='error'>Укажите название кроссворда</div>";}
     
     if($name && $solution && $h && $w){
-        $sel = mysqli_query($db, "SELECT id FROM dk_user WHERE pass='$passProverka' AND kod='$kodProverka'");
+        $sel = mysqli_query($db, "SELECT id,login_view FROM dk_user WHERE pass='$passProverka' AND kod='$kodProverka'");
         if(mysqli_num_rows($sel) > 0){
             $res = mysqli_fetch_assoc($sel);
             $id = $res["id"];
+            $login = $res["login_view"];
             mysqli_real_escape_string($db, $name);
             mysqli_real_escape_string($db, $top);
             mysqli_real_escape_string($db, $left);
@@ -35,6 +36,11 @@
             $sel_proverka =  mysqli_query($db, "SELECT id FROM dk_cross WHERE otvet='$solution'");
             if(mysqli_num_rows($sel_proverka) == 0){            
                 if(mysqli_query($db, "INSERT INTO dk_cross (name,user_add_id,date_add,time_add,cross_h,cross_w,arr_top,arr_left,otvet) VALUES ('$name','$id','$date_add','".TIMES."','$h','$w','$top','$left','$solution')")){
+                    $last_id = mysqli_insert_id($db);
+                    $subject = "Новый кроссворд на сайте: ";
+                    $header = "From: \"".TITLE_SITE."\" <".ADMIN_EMAIL.">";
+                    $message = "<h4>Здравствуйте, администратор!</h4><p>Пользователь <a href='".DOMEN."/user.php?id=".$id."' target='blank'>".$login."</a> добавил новый кроссворд с названием: <strong>".$name."</strong>. Для проверки кроссворда следует перейти по ссылке:<br><a href='".DOMEN."/report.php?cross=".$last_id."' target='blank'>Проверка кроссворда</a></p><p>Данное письмо сгенерировано автоматически. Отвечать на него не надо.<br>Спасибо.</p>";
+                    mail(ADMIN_EMAIL,$subject,$message,$header."\r\nContent-type:text/html;Charset=utf-8\r\n");
                     echo "<div class='error_plus'>Кроссворд добавлен, после проверки он появится на сайте</div>";
                 }
             }else{echo "<div class='error'>Такой кроссворд уже есть в базе!</div>";}
