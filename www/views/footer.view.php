424d070c1ad04938ad4d33361af7b4cb6f8d7a17
kacevnik <kacevnik@yandex.ru>
2017-05-22 20:03:57 +0300

    Динамический вывод даты копирайта в футере

--- www/views/footer.view.php
+++ www/views/footer.view.php
@@ -1,6 +1,6 @@
         <div class="footer">
             <div class="center">
-                <div class="footer_copy">Все права защищены&#169; 2013-2016 <a href="index.php">Японские кроссворды</a></div>
+                <div class="footer_copy">Все права защищены&#169; 2013-<?php echo date("Y"); ?> <a href="index.php">Японские кроссворды</a></div>
                 <div class="footer_right"><a href="ans.php">Обратная связь</a></div>
             </div>
         </div>
