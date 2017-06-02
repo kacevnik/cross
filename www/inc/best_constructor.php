0826ec659086895cb1efbfa6b77a602dad9cdac1
kacevnik <kacevnik@yandex.ru>
2017-06-02 21:08:14 +0300

    Добавил стили для Списка лучших игроков

--- www/inc/best_constructor.php
+++ www/inc/best_constructor.php
@@ -1,7 +1,7 @@
 <?php 
     if(ENTER != 1){echo "УПС, ОБЛОМ"; exit();}
     
-    
+    $od_style = '';
     $res = mysqli_query($db, "SELECT id,reting,login_view FROM dk_user WHERE metka='1' ORDER BY reting DESC LIMIT 20");
     if(mysqli_num_rows($res) > 0){
         $myr = mysqli_fetch_assoc($res);
--- www/views/best.view.php
+++ www/views/best.view.php
@@ -1,7 +1,7 @@
 <?php
     if(ENTER != 1){echo "УПС, ОБЛОМ"; exit();}
 ?>                    
-                    <div class="content">
+                    <div class="content best_list_style">
 <?php
  echo $_SESSION['error'];
  unset($_SESSION['error']);
@@ -11,16 +11,17 @@
                         <h1>Рейтинг Лучших игроков</h1>
                         <div class="best_list">
                             <table>
-                                <tr>
+                                <tr class="tr_header">
                                     <td>Место</td>
                                     <td>Имя</td>
                                     <td>Очки</td>
                                 </tr>
 <?php
     do{
+        if($i%2==0){$od_style = ' class="odd_best_list_item"';}else{$od_style = ''; }
 ?>
-                                <tr>
-                                    <td>#<i class="fa fa-address-book"></i> <?php echo $i; ?></td>
+                                <tr<?php echo $od_style; ?>>
+                                    <td>#<?php echo $i; ?></td>
                                     <td><a href="user.php?id=<?php echo $myr['id']; ?>"><?php echo $myr['login_view']; ?></a></td>
                                     <td><?php echo round($myr['reting'], 2); ?></td>
                                 </tr> 
